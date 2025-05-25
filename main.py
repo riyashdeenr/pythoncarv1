d358456afc25a25f43864adf5661e9fe72a1ff3e
Yash <reiyashdean@gmail.com>
2025-05-25 18:10:55 +0800

    edited the main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 
+from db_utils import initialize_db, verify_password
+from user_login import login_page
 # This code demonstrates the use of global variables in Python.
 # It defines a global variable `int` and modifies it within a function.
 def main():
     global int
     int = 2
 
+
+
 if __name__ == "__main__":
     main()
+    initialize_db()  # Ensure the database is initialized
+    login_page()
     print(int)
 
 # The code defines a global variable 'int' and a function 'main'.print(int)
